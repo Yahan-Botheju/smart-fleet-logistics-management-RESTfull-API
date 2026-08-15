f41dff03b0ca2a54fb1ef6f22c83484c5b5b77ca
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 11:50:28 +0530

    initial commit

---
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/Application.java
@@ -0,0 +1,13 @@
+package lk.modular.monolithic.smart.fleet.logistics.management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

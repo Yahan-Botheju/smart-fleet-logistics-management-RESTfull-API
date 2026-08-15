4b027a0ef3642daac266e68c7401cad9e7ff1cba
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 20:47:41 +0530

    feat: initiate user persistence impl as component class through config class

--- src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/_configs/persistenceBeanConfigs/PersistenceBeanConfigs.java
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/_configs/persistenceBeanConfigs/PersistenceBeanConfigs.java
@@ -1,4 +1,21 @@
 package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure._configs.persistenceBeanConfigs;
 
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.repositories.UserRepository;
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.UserRepositoryImpl;
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.jpa.JpaUserRepository;
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.persistenceMapper.UserPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PersistenceBeanConfigs {
+
+    //user persistence bean config
+    @Bean
+    public UserRepository userRepository(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper
+    ){
+        return new UserRepositoryImpl(jpaUserRepository, userPersistenceMapper);
+    }
 }

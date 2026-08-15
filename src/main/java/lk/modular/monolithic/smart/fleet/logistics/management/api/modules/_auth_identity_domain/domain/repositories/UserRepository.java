5ef2fd5bf03f3262fbb8968e94de2e09a7fbc487
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 20:42:47 +0530

    feat: initiate user domain repo

---
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/domain/repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.repositories;
+
+public interface UserRepository{
+}
---
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
@@ -0,0 +1,4 @@
+package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence;
+
+public class UserRepositoryImpl {
+}

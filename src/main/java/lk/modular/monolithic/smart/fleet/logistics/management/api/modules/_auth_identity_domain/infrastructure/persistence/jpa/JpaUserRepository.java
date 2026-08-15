b945513844f57293dae1e09937b06c124052ac00
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 21:18:18 +0530

    feat: initiate custom query

--- src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/jpa/JpaUserRepository.java
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/jpa/JpaUserRepository.java
@@ -3,7 +3,11 @@ package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth
 import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
+
+    //user find by email
+    Optional<UserEntity> findByEmail(String email);
 }

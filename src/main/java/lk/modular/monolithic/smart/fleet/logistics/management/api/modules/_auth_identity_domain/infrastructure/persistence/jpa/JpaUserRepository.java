410fa333e78b612fbe2fe28a072810d851820ee2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 20:36:02 +0530

    feat: initiate user jpa repo

---
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/jpa/JpaUserRepository.java
@@ -0,0 +1,9 @@
+package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.jpa;
+
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
+}

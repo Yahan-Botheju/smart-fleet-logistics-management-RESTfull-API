5334ba93e315e8b48ebf4f479492b3fdcfde63c7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 21:07:20 +0530

    feat: initiate user save method

--- src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/domain/repositories/UserRepository.java
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/domain/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.repositories;
 
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.models.User;
+
 public interface UserRepository{
+
+    //save user
+    User save(User user);
 }

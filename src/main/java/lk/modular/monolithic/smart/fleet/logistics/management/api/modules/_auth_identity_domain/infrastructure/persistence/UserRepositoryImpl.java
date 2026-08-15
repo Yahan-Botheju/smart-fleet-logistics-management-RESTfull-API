a99bb4b4de28c5db7696618f61c1769e0ad59bc1
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 21:07:44 +0530

    feat: save user in db

--- src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
@@ -1,6 +1,8 @@
 package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence;
 
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.models.User;
 import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.repositories.UserRepository;
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.entities.UserEntity;
 import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.jpa.JpaUserRepository;
 import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.persistenceMapper.UserPersistenceMapper;
 
@@ -16,5 +18,14 @@ public class UserRepositoryImpl implements UserRepository {
     ) {
         this.jpaUserRepository = jpaUserRepository;
         this.userPersistenceMapper = userPersistenceMapper ;
+    }
+
+    //save user
+    @Override
+    public User save(User user) {
+        UserEntity convertEntity = userPersistenceMapper.toEntity(user);
+        UserEntity savedEntity = jpaUserRepository.save(convertEntity);
+
+        return userPersistenceMapper.toDomainModel(savedEntity);
     }
 }

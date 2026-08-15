6538e43694c94b5b56844e1c94300bc1cb5468f0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 20:45:29 +0530

    feat: inject required dependencies

---
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/_configs/persistenceBeanConfigs/PersistenceBeanConfigs.java
@@ -0,0 +1,4 @@
+package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure._configs.persistenceBeanConfigs;
+
+public class PersistenceBeanConfigs {
+}
--- src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
@@ -1,4 +1,19 @@
 package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence;
 
-public class UserRepositoryImpl {
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.repositories.UserRepository;
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.jpa.JpaUserRepository;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    //inject required dependencies
+    private final JpaUserRepository jpaUserRepository;
+    private final UserRepository userRepository;
+
+    public UserRepositoryImpl(
+            JpaUserRepository jpaUserRepository,
+            UserRepository userRepository
+    ) {
+        this.jpaUserRepository = jpaUserRepository;
+        this.userRepository = userRepository;
+    }
 }

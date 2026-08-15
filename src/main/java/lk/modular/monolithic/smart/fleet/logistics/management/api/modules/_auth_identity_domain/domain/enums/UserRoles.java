38cec8ed9ee67ac1db2e8210c0f8ac927c6ce4d0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 19:56:27 +0530

    feat: initiate user roles

---
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/domain/enums/UserRoles.java
@@ -0,0 +1,9 @@
+package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.enums;
+
+public enum UserRoles {
+    ADMIN,
+    DISPATCHER,
+    DRIVER,
+    CUSTOMER,
+    MANAGER
+}

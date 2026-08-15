d1be4104ad8745c58db70e87f4aae4f0f32bca80
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-15 20:46:59 +0530

    fix: remove wrong inject and inject required dependencies

--- src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
+++ src/main/java/lk/modular/monolithic/smart/fleet/logistics/management/api/modules/_auth_identity_domain/infrastructure/persistence/UserRepositoryImpl.java
@@ -2,18 +2,19 @@ package lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth
 
 import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.domain.repositories.UserRepository;
 import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.jpa.JpaUserRepository;
+import lk.modular.monolithic.smart.fleet.logistics.management.api.modules._auth_identity_domain.infrastructure.persistence.persistenceMapper.UserPersistenceMapper;
 
 public class UserRepositoryImpl implements UserRepository {
 
     //inject required dependencies
     private final JpaUserRepository jpaUserRepository;
-    private final UserRepository userRepository;
+    private final UserPersistenceMapper userPersistenceMapper;
 
     public UserRepositoryImpl(
             JpaUserRepository jpaUserRepository,
-            UserRepository userRepository
+            UserPersistenceMapper userPersistenceMapper
     ) {
         this.jpaUserRepository = jpaUserRepository;
-        this.userRepository = userRepository;
+        this.userPersistenceMapper = userPersistenceMapper ;
     }
 }
